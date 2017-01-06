3046624bf222207731389c07224c8c4cfbdc3239
Parsons-David <david.parsons@rutgers.edu>
2017-01-06 15:56:51 -0500

    Added test shell script

---
+++ test.sh
@@ -0,0 +1,8 @@
+# Compile all Java files into the build folder.
+javac -d build *.java
+
+# Enter build directory. *I couldn't get it to run from a higher directory*
+cd build
+
+# Run Train, which contains the main method for the project.
+java Train
