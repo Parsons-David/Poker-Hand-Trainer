eaa038fe2d7b674d1400bf467665c9c1cb62c872
Parsons-David <david.parsons@rutgers.edu>
2017-01-13 23:38:00 -0500

    Cleaned up Main Class

--- Train.java
+++ Train.java
@@ -5,43 +5,7 @@ public class Train {
 
   public static void main(String[] args) {
 
-    // Window Testing
-
     TrainWindow window = new TrainWindow();
-
-    // END Window Testing
-
-
-    // Deck Testing
-
-    // Deck test = new Deck();
-    // test.print();
-    // test.shuffle();
-    // System.out.println();
-    // test.print();
-    // test.shuffle();
-    //
-    // System.out.println();
-    // for(int i = 0; i < 200; i++){
-    //   System.out.println(test.drawCard().toString());
-    // }
-
-    // END Deck Testing
-
-    // Equality and Contains Test
-
-    // ArrayList<Card> test = new ArrayList<>();
-    // for(int i = 1; i < 8; i++){
-    //   Card hold = new Card(0, i);
-    //   test.add(hold);
-    //   System.out.println(test.get(i - 1).toString());
-    // }
-    //
-    // for(int i = 5; i < 10; i++){
-    //   System.out.println(test.contains(new Card(0, i)));
-    // }
-
-    // END Equality and Contains Test
 
   }
 
