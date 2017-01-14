b4f71ef11c24647e495175c584b28f410779a260
Parsons-David <david.parsons@rutgers.edu>
2017-01-14 15:02:02 -0500

    Updated Test Script

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 # Compile all Java files into the build folder.
-javac -d build *.java
+javac -d build/ClassFiles src/*.java
 
 # Enter build directory. *I couldn't get it to run from a higher directory*
-cd build
+cd build/ClassFiles
 
 # Run Train, which contains the main method for the project.
 java Train
