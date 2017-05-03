0890d43d51d043086f63166339e293a3890124a9
Parsons-David <david.parsons@rutgers.edu>
2017-05-03 18:54:50 -0500

    Started on Table State : State Button, Community Cards, Hold Cards all update on State Button Press

--- Android/PokerTrainer/app/src/main/java/me/parsonssoftware/pokertrainer/MainMenu.java
+++ Android/PokerTrainer/app/src/main/java/me/parsonssoftware/pokertrainer/MainMenu.java
@@ -1,13 +1,42 @@
 package me.parsonssoftware.pokertrainer;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class MainMenu extends AppCompatActivity {
+public class MainMenu extends Activity {
+
+    private Button btnTrain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_menu);
+
+        btnTrain = (Button) findViewById(R.id.btnTrain);
+
     }
+
+    // Action Handler for Train Button on Main Menu
+    public void onTrainPress(View v){
+
+        // Assures the Train Button is being pressed
+        if((Button) v != btnTrain){
+            Toast.makeText(getApplicationContext(), "This Button Should not Launch Table.", Toast.LENGTH_SHORT).show();
+        }
+
+        showTable();
+
+    }
+
+    // Move to Table Activity
+    void showTable(){
+        Intent intent = new Intent(this, Table.class);
+        startActivity(intent);
+    }
+
 }
