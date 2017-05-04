e8a62133938707c5dbbd52bed069eaf27f943508
Parsons-David <david.parsons@rutgers.edu>
2017-05-03 22:57:59 -0500

    Added Hand Buttons Control

--- Android/PokerTrainer/app/src/main/java/me/parsonssoftware/pokertrainer/control/Session.java
+++ Android/PokerTrainer/app/src/main/java/me/parsonssoftware/pokertrainer/control/Session.java
@@ -11,36 +11,107 @@ import me.parsonssoftware.pokertrainer.R;
 public class Session {
 
     private int state = 0;
+    private boolean[] allHands = new boolean[8];
+    private boolean[] yourHands = new boolean[8];
 
     private final int cardBack = R.drawable.b;
 
-    public Bundle nextState(){
-        Bundle currentState = new Bundle();
+    // COMMENT
+    public void nextState(){
 
-        currentState.putInt("CardBack", cardBack);
+        if(state == 0){
+            // This needs to be handled elsewhere
+            resetHands();
+        }
+        if(state == 3){
+            // This needs to be handled elsewhere
+            clearHands();
+            state = 0;
+        } else {
+            state += 1;
+        }
+    }
 
-        String stateRep;
-        int cCount = 0;
-        int hCount = 2;
+    // COMMENT
+    private void resetHands(){
+        for(int i = 0; i < 8; i++){
+            allHands[i] = true;
+            yourHands[i] = true;
+        }
+    }
 
-        int[] CIds = {R.drawable.c3, R.drawable.c6, R.drawable.h4, R.drawable.s5, R.drawable.c8};
-        int[] HIds = {R.drawable.cj, R.drawable.d9};
+    // COMMENT
+    private void clearHands(){
+        for(int i = 0; i < 8; i++){
+            allHands[i] = false;
+            yourHands[i] = false;
+        }
+    }
+
+    // Returns a string representing the sessions current state.
+    public String getStateRepresentation(){
+        switch (state){
+            case 0 :  return "Press to Start";
+            case 1 : return "Turn";
+            case 2 : return "River";
+            case 3 : return "Finish";
+            default: return "Error";
+        }
+    }
+
+    // Returns integer representing the image resources of the community cards currently visible
+    public int[] getCommunityImageIDs(){
+
+        int[] ids = {R.drawable.c3, R.drawable.c6, R.drawable.h4, R.drawable.s5, R.drawable.c8};
+
+        int count = 0;
 
         switch (state){
-            case 0 : stateRep = "Turn"; cCount = 3; break;
-            case 1 : stateRep = "River"; cCount = 4; break;
-            case 2 : stateRep = "Finish"; cCount = 5; break;
-            case 3 : state = -1; stateRep = "Press to Start"; cCount = 0; hCount = 0; break;
-            default: stateRep = "Error"; cCount = 0;
+            case 0 : count = 0; break;
+            case 1 : count = 3; break;
+            case 2 : count = 4; break;
+            case 3 : count = 5; break;
+            default: count = 0;
         }
 
-        currentState.putString("State", stateRep);
-        currentState.putInt("cCount", cCount);
-        currentState.putInt("hCount", hCount);
-        currentState.putIntArray("Cids", CIds);;
-        currentState.putIntArray("Hids", HIds);
+        for(int i = count; i < 5; i++){
+            ids[i] = cardBack;
+        }
 
-        state += 1;
-        return currentState;
+        return ids;
     }
+
+    // Returns integer representing the image resources of the hold cards currently visible
+    public int[] getHoldImageIDs(){
+
+        int[] ids = {R.drawable.cj, R.drawable.d9};
+
+        int count = (state == 0 ? 0 : 2);
+
+        for(int i = count; i < 2; i++){
+            ids[i] = cardBack;
+        }
+
+        return ids;
+    }
+
+    // COMMENT
+    public void removeHand(int hand, boolean isAll){
+        if(isAll){
+            allHands[hand] = false;
+        } else {
+            yourHands[hand] = false;
+        }
+    }
+
+    // COMMENT
+    public boolean[] getAllHands(){
+        return allHands;
+    }
+
+    // COMMENT
+    public boolean[] getYourHands(){
+        return yourHands;
+    }
+
 }
--- Android/PokerTrainer/app/src/main/java/me/parsonssoftware/pokertrainer/view/Table.java
+++ Android/PokerTrainer/app/src/main/java/me/parsonssoftware/pokertrainer/view/Table.java
@@ -8,17 +8,23 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 import me.parsonssoftware.pokertrainer.R;
 import me.parsonssoftware.pokertrainer.control.Session;
 
 public class Table extends Activity {
 
     private Button btnState;
+    private Button btnToggleHands;
+    private ArrayList<Button> btnHands = new ArrayList<>();
+
 
     private ImageView[] communityImages = new ImageView[5];
     private ImageView[] holdImages = new ImageView[2];
 
     private Session session = new Session();
+    private boolean viewingAll = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +32,26 @@ public class Table extends Activity {
         setContentView(R.layout.table);
 
         btnState = (Button) findViewById(R.id.btnState);
+        btnToggleHands = (Button) findViewById(R.id.btnToggleHands);
+
+        btnHands.add((Button) findViewById(R.id.btnSF));
+        btnHands.add((Button) findViewById(R.id.btnFH));
+        btnHands.add((Button) findViewById(R.id.btnStr));
+        btnHands.add((Button) findViewById(R.id.btnTP));
+        btnHands.add((Button) findViewById(R.id.btnFK));
+        btnHands.add((Button) findViewById(R.id.btnFl));
+        btnHands.add((Button) findViewById(R.id.btnTK));
+        btnHands.add((Button) findViewById(R.id.btnP));
+
+        for(Button btn : btnHands){
+            btn.setOnClickListener(new Button.OnClickListener() {
+                public void onClick(View v) {
+                    onHandPress(v);
+                }
+            });
+        }
+
+        updateHandButtons();
 
         communityImages[0] = (ImageView) findViewById(R.id.imgViewC1);
         communityImages[1] = (ImageView) findViewById(R.id.imgViewC2);
@@ -36,6 +62,8 @@ public class Table extends Activity {
         holdImages[0] = (ImageView) findViewById(R.id.imgViewH1);
         holdImages[1] = (ImageView) findViewById(R.id.imgViewH2);
 
+
+        session.nextState();
     }
 
     // Action Handler for State Button on Table
@@ -44,29 +72,69 @@ public class Table extends Activity {
         // Assures the Train Button is being pressed
         if((Button) v != btnState){
             Toast.makeText(getApplicationContext(), "This Button Should not Change State", Toast.LENGTH_SHORT).show();
+            return;
         }
-
-        // Gets state date from current session
-        Bundle newState = session.nextState();
 
         // Updates State Button
-        btnState.setText(newState.getString("State"));
-
+        btnState.setText(session.getStateRepresentation());
 
         // Updates Community Card Images
-        for(int i = 0; i < newState.getInt("cCount"); i++){
-            communityImages[i].setImageResource(newState.getIntArray("Cids")[i]);
-        }
-        for(int i = newState.getInt("cCount"); i < 5; i++){
-            communityImages[i].setImageResource(newState.getInt("CardBack"));
+        for(int i = 0; i < 5; i++){
+            communityImages[i].setImageResource(session.getCommunityImageIDs()[i]);
         }
 
         // Updates Hold Card Images
-        for(int i = 0; i < newState.getInt("hCount"); i++){
-            holdImages[i].setImageResource(newState.getIntArray("Hids")[i]);
+        for(int i = 0; i < 2; i++){
+            holdImages[i].setImageResource(session.getHoldImageIDs()[i]);
         }
-        for(int i = newState.getInt("hCount"); i < 2; i++){
-            holdImages[i].setImageResource(newState.getInt("CardBack"));
+
+        updateHandButtons();
+
+        session.nextState();
+
+    }
+
+    // Action Handler for Toggle Hand Button on Table
+    public void onToggleHandsPress(View v){
+
+        // Assures the Toggle Button is being pressed
+        if((Button) v != btnToggleHands){
+            Toast.makeText(getApplicationContext(), "This Button Should not Toggle Hands", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // Updates what is being viewed
+        viewingAll = !viewingAll;
+
+        // Toggles Test Based on what is currently being viewed
+        btnToggleHands.setText(viewingAll ? R.string.allHands : R.string.yourHands );
+
+        updateHandButtons();
+    }
+
+
+    // COMMENT
+    public void onHandPress(View v){
+        Button pressed = (Button) v;
+        // Assures the Toggle Button is being pressed
+        if(! btnHands.contains(pressed)){
+            Toast.makeText(getApplicationContext(), "This Button Should not Toggle Hands", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        pressed.setEnabled(false);
+
+        session.removeHand(btnHands.indexOf(pressed), viewingAll);
+
+    }
+
+    // COMMENT
+    private void updateHandButtons(){
+
+        boolean[] visibility = (viewingAll ? session.getAllHands() : session.getYourHands() );
+
+        for(int i = 0; i < visibility.length; i++){
+            btnHands.get(i).setEnabled(visibility[i]);
         }
 
     }
